c7bb845369a96653a7449dce0440f1b81abbda64
Santiago0303afk <santiago.cadme71@myhunter.cuny.edu>
2025-05-09 21:55:41 -0400

    students.js

--- routes/students.js
+++ routes/students.js
@@ -71,32 +71,3 @@ router.put('/:id', ash(async(req, res) => {
 module.exports = router;
 
 
-//sc added:
-const Sequelize = require('sequelize');
-const db = require('../db');
-
-const Student = db.define("student", {
-  firstname: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  lastname: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  email: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    validate: { isEmail: true }
-  },
-  imageUrl: {
-    type: Sequelize.STRING,
-    defaultValue: 'https://via.placeholder.com/150'
-  },
-  gpa: {
-    type: Sequelize.DECIMAL(2, 1),
-    validate: { min: 0.0, max: 4.0 }
-  }
-});
-
-module.exports = Student;
